487bafd147f1cb5d6215851689d607f6d7dce2b8
Mal Morrow <malmorrow@gmail.com>
2021-07-10 10:05:34 +0200

    Added a test function.

--- mobile_sectors/greatcircle.py
+++ mobile_sectors/greatcircle.py
@@ -1,4 +1,4 @@
-def location_at_distance(start_lat, start_lon, distance, direction):
+def location_at_distance(start_lon, start_lat, direction, distance=5):
     '''
     The purpose of this is to find a latitude and longitude at a specific distance from 
     another point. The inputs are the starting latitude and longitude, and the distance
@@ -29,3 +29,25 @@ def location_at_distance(start_lat, start_lon, distance, direction):
         end_lon=((start_lon-asin(sin(direction)*sin(distance)/cos(end_lat))+pi)%(2*pi))-pi
         
     return end_lat/radians, end_lon/radians
+
+def test():
+    import pandas as pd
+
+    # Different distances in three bearings from a point near Cape Town.
+    sites = [
+        [18.565784, -33.603048, 0, 5],
+        [18.565784, -33.603048, 120, 50],
+        [18.565784, -33.603048, 240, 500],
+    ]
+
+    df = pd.DataFrame(sites, columns=['long', 'lat', 'azimuth', 'distance'])
+
+    applied_df = \
+        df.apply(lambda x: location_at_distance(x.long, x.lat, x.azimuth), axis='columns', result_type='expand') \
+            .rename(columns={0: 'new_long', 1: 'new_lat'})
+    df = pd.concat([df, applied_df], axis='columns')
+
+    return df
+
+if __name__ == '__main__':
+    test()
